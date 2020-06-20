24a15e3a68ed47788bf4d778e8146b3cc6aed889
chendb <kjgfcdb@126.com>
2020-06-20 20:54:23 +0800

    v0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="diffdir",
-    version="0.3",
+    version="0.4",
     description="Diff directories",
     license="MIT Licence",
     author="cdb",
